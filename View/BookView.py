253c81c980cf6d3136aa86f34d8ae1db3eca1109
raiana-santana <raiana28santana@gmail.com>
2024-05-12 20:36:43 -0300

    alterações

--- View/BookView.py
+++ View/BookView.py
@@ -1,6 +1,6 @@
 #Aqui será implementado os métodos de Criar, Exibir, Atualizar e Deletar os Livros.
 from Model.Book import Book
-# from Controller.BookController import Book
+from Controller.BookController import BookController
 
 class BookView:
     def update_book(self):
