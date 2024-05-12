05fe0591b2ead3dfbf45d4747baea968814e050d
raiana-santana <raiana28santana@gmail.com>
2024-05-12 20:11:17 -0300

    Criando a classe BookController

--- Controller/BookController.py
+++ Controller/BookController.py
@@ -2,3 +2,11 @@
 
 from Model.Book import Book
 from DAO.Database import Database
+
+#Criação da classe BookController, onde fazemos adição na lista books com o .append, e retornamos uma cópia dessa lista com o .copy()
+class BookController:
+    def save(self, book):
+        Database.books.append(book)
+
+    def show_all(self):
+        return Database.books.copy()
