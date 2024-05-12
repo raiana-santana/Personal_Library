75ffa527fba05667ee27eebd521e49dace7d0a71
raiana-santana <raiana28santana@gmail.com>
2024-05-12 20:03:13 -0300

    Criando a classe BookView

--- View/BookView.py
+++ View/BookView.py
@@ -1,1 +1,7 @@
 #Aqui será implementado os métodos de Criar, Exibir, Atualizar e Deletar os Livros.
+from Model.Book import Book
+# from Controller.BookController import Book
+
+class BookView:
+    def update_book(self):
+        ...
